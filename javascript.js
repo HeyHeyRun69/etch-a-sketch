dd6355e5ca860d48149640bb25c8eb091c9fdc2d
Aaron Saucedo <ajsm.0596@gmail.com>
2025-04-22 15:37:32 -0600

    feat: Finish final steps

--- javascript.js
+++ javascript.js
@@ -1,7 +1,33 @@
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('.new-grid-button');
+
+resetButton.addEventListener('click', () => {
+    let numberOfBoxes = parseInt(prompt('How many pixels per side do you want?'));
+    do {
+        alert('More than 100 boxes is UNFEASIBLE');
+        numberOfBoxes = parseInt(prompt('How many pixels per side do you want?'));
+    } while (numberOfBoxes > 100);
+    console.log();
+
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for(i = 0; i < (numberOfBoxes * numberOfBoxes) + numberOfBoxes; i++){
+        const div = document.createElement('div');
+        container.appendChild(div);
+        document.querySelectorAll(`.container>div:nth-child(${numberOfBoxes + 1}n + 1)`).forEach((item) => {
+            item.setAttribute('id', 'sketch-item');
+        })
+    }
+})
 
 
 for(i = 0; i < 256 + 16; i++){
     const div = document.createElement('div');
     container.appendChild(div);
 }
+
+document.querySelectorAll(`.container>div:nth-child(17n + 1)`).forEach((item) => {
+    item.setAttribute('id', 'sketch-item');
+})
